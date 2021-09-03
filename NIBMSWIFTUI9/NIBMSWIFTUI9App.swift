4cab4fceab842cf8b2fdbd32735c62bff61e6973
Thushal Madhushankha <thushal@encyteus.com>
2021-09-03 17:15:28 +0530

    Initial Commit

---
+++ NIBMSWIFTUI9/NIBMSWIFTUI9App.swift
@@ -0,0 +1,17 @@
+//
+//  NIBMSWIFTUI9App.swift
+//  NIBMSWIFTUI9
+//
+//  Created by Thushal Madhushankha on 2021-09-03.
+//
+
+import SwiftUI
+
+@main
+struct NIBMSWIFTUI9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
